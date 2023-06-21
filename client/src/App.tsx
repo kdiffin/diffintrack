a42599b15672f7802123a9960b55a7cf8d4126a5
JeweIxx <vhiunese@gmail.com>
2023-06-21 21:05:15 +0400

    homepage initialized

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,37 +1,12 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import Homepage from "./components/Homepage";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [test, setTest] = useState();
-
-  useEffect(() => {
-    fetch("http://127.0.0.1:8000/bru")
-      .then((response) => response.json())
-      .then((data) => setTest(data));
-  }, []);
-
-  console.log(test);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">{test}</p>
-    </>
+    <div className="bg-gray-900 flex px-8 pb-4  flex-col background-homepage text-white min-h-screen">
+      <Navbar />
+      <Homepage />
+    </div>
   );
 }
 
