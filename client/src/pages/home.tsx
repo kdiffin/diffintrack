ce0234f282204ebf48fb9d0f9e3d5d30c26f5ad1
JeweIxx <vhiunese@gmail.com>
2023-07-01 01:43:51 +0400

    added first statistics  for workout accuracy

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -4,6 +4,7 @@ import Card from "~/components/ui/Card";
 import withLayout from "~/components/Layout";
 import Input from "~/components/ui/Input";
 import { Username } from "~/components/ui/UserAttributes";
+import { MdHandshake } from "react-icons/md";
 
 function home() {
   const { user } = useUser();
@@ -20,149 +21,35 @@ function home() {
 
       <div className="  flex gap-10 overflow-x-auto pb-3  ">
         <ChartContainer
-          title="bro"
-          icon={<div>a</div>}
+          title="Arms"
+          icon={<MdHandshake size={30} />}
           increase="20"
           progress="40"
-          subtitle="yooooo"
-          className="p-8"
         />
         <ChartContainer
-          title="bro"
-          icon={<div>a</div>}
+          title="Chest"
+          icon={<MdHandshake size={30} />}
           increase="20"
           progress="40"
-          subtitle="yooooo"
-          className="p-8"
         />
         <ChartContainer
-          title="bro"
-          icon={<div>a</div>}
+          title="Back"
+          icon={<MdHandshake size={30} />}
           increase="20"
           progress="40"
-          subtitle="yooooo"
-          className="p-8"
         />
         <ChartContainer
-          title="bro"
-          icon={<div>a</div>}
+          title="Shoulder"
+          icon={<MdHandshake size={30} />}
           increase="20"
           progress="40"
-          subtitle="yooooo"
-          className="p-8"
-        />
-        <ChartContainer
-          title="bro"
-          icon={<div>a</div>}
-          increase="20"
-          progress="40"
-          subtitle="yooooo"
-          className="p-8"
-        />
-        <ChartContainer
-          title="bro"
-          icon={<div>a</div>}
-          increase="20"
-          progress="40"
-          subtitle="yooooo"
-          className="p-8"
         />{" "}
         <ChartContainer
-          title="bro"
-          icon={<div>a</div>}
+          title="Legs"
+          icon={<MdHandshake size={30} />}
           increase="20"
           progress="40"
-          subtitle="yooooo"
-          className="p-8"
         />{" "}
-        <ChartContainer
-          title="bro"
-          icon={<div>a</div>}
-          increase="20"
-          progress="40"
-          subtitle="yooooo"
-          className="p-8"
-        />{" "}
-        <ChartContainer
-          title="bro"
-          icon={<div>a</div>}
-          increase="20"
-          progress="40"
-          subtitle="yooooo"
-          className="p-8"
-        />{" "}
-        <ChartContainer
-          title="bro"
-          icon={<div>a</div>}
-          increase="20"
-          progress="40"
-          subtitle="yooooo"
-          className="p-8"
-        />{" "}
-        <ChartContainer
-          title="bro"
-          icon={<div>a</div>}
-          increase="20"
-          progress="40"
-          subtitle="yooooo"
-          className="p-8"
-        />{" "}
-        <ChartContainer
-          title="bro"
-          icon={<div>a</div>}
-          increase="20"
-          progress="40"
-          subtitle="yooooo"
-          className="p-8"
-        />{" "}
-        <ChartContainer
-          title="bro"
-          icon={<div>a</div>}
-          increase="20"
-          progress="40"
-          subtitle="yooooo"
-          className="p-8"
-        />{" "}
-        <ChartContainer
-          title="bro"
-          icon={<div>a</div>}
-          increase="20"
-          progress="40"
-          subtitle="yooooo"
-          className="p-8"
-        />{" "}
-        <ChartContainer
-          title="bro"
-          icon={<div>a</div>}
-          increase="20"
-          progress="40"
-          subtitle="yooooo"
-          className="p-8"
-        />{" "}
-        <ChartContainer
-          title="bro"
-          icon={<div>a</div>}
-          increase="20"
-          progress="40"
-          subtitle="yooooo"
-          className="p-8"
-        />{" "}
-        <ChartContainer
-          title="bro"
-          icon={<div>a</div>}
-          increase="20"
-          progress="40"
-          subtitle="yooooo"
-          className="p-8"
-        />
-        <ChartContainer
-          title="bro"
-          icon={<div>a</div>}
-          increase="20"
-          progress="40"
-          subtitle="yooooo"
-          className="p-8"
-        />
       </div>
     </div>
   );
@@ -171,54 +58,68 @@ function home() {
 function ChartContainer({
   className,
   title,
-  subtitle,
   progress,
   increase,
   icon,
 }: {
   className?: string;
   title: string;
-  subtitle: string;
   progress: string;
   increase: string;
   icon: React.ReactNode;
 }) {
   return (
     <div
-      className={`${className} flex items-center justify-center bg-indigo-900/20 `}
+      className={`${className} flex items-center justify-center bg-indigo-900/20 px-6 py-4 `}
     >
-      <div className="w-full">
-        <div className="flex justify-between">
-          <div>
-            {icon}
+      <div className="mr-20 flex flex-col gap-2">
+        <div>{icon}</div>
 
-            <div className="font-bold ">{title}</div>
-          </div>
+        <div className="text-3xl font-semibold">{title}</div>
 
-          <div>
-            <ProgressCircle size="40" progress={progress} />
-          </div>
+        <p className=" min-w-max font-medium text-indigo-300">
+          Workout Accuracy
+        </p>
+      </div>
+
+      <div className="  flex flex-col justify-between gap-6">
+        <div>
+          <ProgressCircle progress={50} />
         </div>
 
-        <div className=" flex justify-between">
-          <p>{subtitle}</p>
-
-          <p className="italic">{increase}</p>
-        </div>
+        <p className="italic text-indigo-300">+{increase}%</p>
       </div>
     </div>
   );
 }
 
-const ProgressCircle = ({
-  progress = "0.75",
-  size = "40",
-}: {
-  progress: string;
-  size: string;
-}) => {
-  const angle = parseInt(progress) * 360;
-  return <div />;
+const ProgressCircle = ({ radius = 20123123, progress = 500 }) => {
+  const circleSize = radius * 2;
+  const circumference = circleSize * Math.PI;
+  const offset = circumference - (progress / 100) * circumference;
+
+  return (
+    <div className="relative h-24 w-24">
+      <div className="relative">
+        <div
+          className="h-full w-full overflow-hidden rounded-full border-8 border-blue-500 border-opacity-50"
+          style={{ position: "relative" }}
+        >
+          <div
+            className="absolute left-0 top-0 h-full w-full border-8 border-blue-500"
+            style={{
+              clipPath: `polygon(0 0, 0% 0%, 0% 100%, ${
+                (offset / circumference) * 100
+              }% 100%, ${(offset / circumference) * 100}% 0)`,
+            }}
+          />
+        </div>
+        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform text-center text-xl text-blue-500">
+          {progress}%
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default withLayout(home);
