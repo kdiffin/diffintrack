6b6767981b2f81ae0ce661c465e00e09b6276ec8
JeweIxx <vhiunese@gmail.com>
2023-07-02 03:40:02 +0400

    added stuff to row 3

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -4,10 +4,11 @@ import Card from "~/components/ui/Card";
 import withLayout from "~/components/Layout";
 import Input from "~/components/ui/Input";
 import { Username } from "~/components/ui/UserAttributes";
-import { MdHandshake, MdHistory } from "react-icons/md";
+import { MdHandshake, MdHistory, MdMail } from "react-icons/md";
 import { CALENDAR_DATA } from "~/mockdata";
 import CalendarComponent from "~/components/Calendar";
 import { divergingColorInterpolators } from "@nivo/colors";
+import { FaDiscord, FaGithub, FaInstagram } from "react-icons/fa";
 
 function home() {
   const { user } = useUser();
@@ -59,7 +60,11 @@ function home() {
         {/* ROW 2 */}
         <div className=" flex flex-col gap-5 xl:flex-row">
           {/* <CalendarComponent data={CALENDAR_DATA} /> */}
-          <div className="flex-1 bg-primary p-6  xl:flex-[0.65] ">a</div>
+          <div className="flex-1 bg-primary p-6  xl:flex-[0.65] ">
+            <p className="flex items-center gap-2  font-medium ">
+              Workout Calendar
+            </p>
+          </div>
 
           {/* RECENT WORKOUTS */}
           <div className="subtle-scrollbar flex max-h-64 flex-col gap-2 overflow-y-auto pr-2  xl:flex-[0.35]">
@@ -88,32 +93,56 @@ function home() {
         <div className=" flex flex-col gap-5 xl:flex-row">
           {/* WORKOUT ACCURACY */}
           <div className="  flex  flex-1 flex-col bg-primary p-6   ">
-            <p className="flex items-center gap-2  font-medium ">
-              Overall workout accuracy
+            <p className="flex items-center gap-2 font-medium  capitalize ">
+              Overall workout rating
             </p>
 
             <div className="flex flex-col items-center justify-center ">
-              <div className="my-3 flex h-36 w-36 items-center justify-center rounded-full border-4 border-accent text-2xl font-semibold">
+              <div className="my-4 flex h-36 w-36 items-center justify-center rounded-full border-4 border-accent text-2xl font-semibold">
                 A+
               </div>
 
-              <p className=" mt-2 text-accent ">
-                Doing great buddy, keep it up!
-              </p>
+              <p className="  mt-1  ">Doing great buddy, keep it up!</p>
             </div>
           </div>
-
           {/* SOCIALS */}
-          <div className="  flex  flex-1 flex-col bg-primary px-6 py-8   ">
-            <p className="flex items-center gap-2  font-medium ">
+          <div className="  flex  flex-1 flex-col bg-primary p-6   ">
+            <p className="flex items-center gap-2 font-medium  capitalize ">
               Where to contact me
             </p>
-          </div>
 
+            <div className="mt-4 flex flex-col  justify-center ">
+              <div className="flex items-center gap-2  p-[6px]  ">
+                <FaGithub className=" text-accent" size={22} />{" "}
+                <p className="    font-medium  ">Github</p>
+              </div>
+
+              <div className="my-2 h-[1px] bg-secondary" />
+
+              <div className=" flex items-center gap-2 p-[6px] ">
+                <FaDiscord className=" text-accent" size={22} />{" "}
+                <p className="    font-medium  ">Discord</p>
+              </div>
+
+              <div className="my-2 h-[1px] bg-secondary" />
+
+              <div className="flex items-center gap-2 p-[6px]  ">
+                <MdMail className=" text-accent" size={22} />{" "}
+                <p className="   font-medium  ">Mail</p>
+              </div>
+
+              <div className="my-2 h-[1px] bg-secondary" />
+
+              <div className="flex items-center gap-2 p-[6px]  ">
+                <FaInstagram className=" text-accent" size={22} />{" "}
+                <p className="  font-medium  ">Instagram</p>
+              </div>
+            </div>
+          </div>
           {/* CONTINUE TO WORKOUTS */}
-          <div className="  flex  flex-1 flex-col bg-primary px-6 py-8  ">
-            <p className="flex items-center gap-2  font-medium ">
-              Done tracking your stats?
+          <div className="  flex  flex-1 flex-col bg-primary p-6  ">
+            <p className="flex items-center gap-2 font-medium  capitalize ">
+              Minutes stretched this week
             </p>
           </div>
         </div>
